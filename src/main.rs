2091252ac0d47ae05d1bdee9ddfa3bb97c414321
Alexander Makedonski <alexandermakedonski@Alexanders-MacBook-Pro-2.local>
2023-10-11 09:54:23 +0300

    add yield

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,63 @@ use tokio::time;
 
 #[tokio::main]
 async fn main() {
-    let mut interval: time::Interval = time::interval(Duration::from_secs(5));
-    tokio::spawn(async move {
+    if let Err(e) = tokio::spawn(async move {
+        task1().await;
+    })
+    .await
+    {
+        println!("{:?}", e);
+    };
+    // loop {
+    //     println!("start task");
+    //     if let Err(e) = tokio::spawn(async move {
+    //         request().await;
+    //     })
+    //     .await {
+    //         println!("{:?}", e);
+    //     }
+    //     println!("end task")ƒ;
+    // }
+}
+
+async fn task1() {
+    let mut interval: time::Interval = time::interval(Duration::from_secs(10));
+    if let Err(e) = tokio::spawn(async move {
         loop {
             interval.tick().await;
             println!("start task");
             request().await;
             println!("end task");
+            tokio::task::yield_now().await;
         }
-    })
-    .await
-    .unwrap();
+    }).await {
+        println!("{:?}", e);
+    }
 }
 
 async fn request() {
-    for _ in 0..100 {
-        tokio::spawn(async move {
-            let url = "https://tokio.rs";
-            let client = Client::builder()
-                .timeout(Duration::from_secs(300))
-                .build()
-                .unwrap();
+    let mut ks = vec![];
+    for _i in 0..100 {
+        ks.push(tokio::spawn(async move {
+            let url = "https://api.bulgarianhistory.shop";
+            let client = Client::builder().timeout(Duration::from_secs(60)).build();
+            match client {
+                Ok(c) => {
+                    if let Err(e) = c.get(url).send().await {
+                        println!("{:?}", e);
+                    };
+                }
+                Err(e) => {
+                    println!("{:?}", e);
+                }
+            }
+            tokio::task::yield_now().await;
+        }));
+    }
 
-            client.get(url).send().await.unwrap().text().await.unwrap();
-        });
+    for k in ks {
+        if let Err(e) = k.await {
+            println!("{:?}", e);
+        };
     }
 }
